23dc479f53aee27cd7487b603712b0ce627f60ab
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-06-17 13:59:54 +0200

    edit app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
+
 
 const PORT = process.env.PORT || 4001;
 
 app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.listen(PORT);
 
 module.exports = app;
