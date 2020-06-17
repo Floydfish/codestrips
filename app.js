57c39184055e72bb943f41e1ec215b34dd767bea
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-06-17 19:29:39 +0200

    Connecting the DB with the routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
+
 const app = express();
 
 
@@ -12,6 +15,16 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.listen(PORT, () => {
     `Server is listening on port: ${PORT}`
+});
+
+app.get('/strips', (req, res, next) => {
+    db.all('SELECT * FROM Strip', (err, rows) => {
+        if (err) {
+            res.sendStatus(500); // Internal server error
+        } else {
+            res.send({ strips: rows });
+        }
+    })
 });
 
 module.exports = app;
